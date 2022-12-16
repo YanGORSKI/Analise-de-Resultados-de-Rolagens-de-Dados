81a9fa87d3f5ea8beb404b004a917df6250d5fd5
Yan Gorski <gorski.yan.93@gmail.com>
2022-12-16 09:48:41 -0300

    add input de num de dados e num de lados

--- src/br/dio/AnaliseResultadoRolagensDedDados.java
+++ src/br/dio/AnaliseResultadoRolagensDedDados.java
@@ -1,75 +1,48 @@
 package br.dio;
 
+import java.sql.Array;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Random;
+import java.util.Scanner;
+import java.util.Set;
 public class AnaliseResultadoRolagensDedDados {
 
 	public static void main(String[] args) {
 
 		Random rng = new Random();
+		Scanner sc = new Scanner(System.in);
 		
 		//100 rolagens e armazenamentos de d6s em sistema de Map
-		System.out.println("--Rolando 100d6--\n");
-		Map<String, Double> rolagensDado = new HashMap<>();
-		for(int i=0; i<100; i++) {
-			int rolagem = rng.nextInt(1, 7);
-			rolagensDado.put(String.valueOf(i+1), (double) rolagem);
-		}
-		
-		//iniciadas variáveis de controle
-		String r1 = "1: ";
-		int n1 = 0;
-		String r2 = "2: ";
-		int n2 = 0;
-		String r3 = "3: ";
-		int n3 = 0;
-		String r4 = "4: ";
-		int n4 = 0;
-		String r5 = "5: ";
-		int n5 = 0;
-		String r6 = "6: ";
-		int n6 = 0;
-		
-		//análise e computação dos resultados
-		for (Map.Entry<String, Double> entry: rolagensDado.entrySet()) {
-			if(entry.getValue().equals(1.0)) {
-				r1 += "■";
-				n1 += 1;
-			}
-			if(entry.getValue().equals(2.0)) {
-				r2 += "■";
-				n2 += 1;
-			}
-			if(entry.getValue().equals(3.0)) {
-				r3 += "■";
-				n3 += 1;
-			}
-			if(entry.getValue().equals(4.0)) {
-				r4 += "■";
-				n4 += 1;
-			}
-			if(entry.getValue().equals(5.0)) {
-				r5 += "■";
-				n5 += 1;
-			}
-			if(entry.getValue().equals(6.0)) {
-				r6 += "■";
-				n6 += 1;
-			}
-		}
-		
-		//exibição dos resultados
-		System.out.println("--Números rolados--");
-		System.out.println(r1 + " ("+n1+")");
-		System.out.println(r2 + " ("+n2+")");
-		System.out.println(r3 + " ("+n3+")");
-		System.out.println(r4 + " ("+n4+")");
-		System.out.println(r5 + " ("+n5+")");
-		System.out.println(r6 + " ("+n6+")");
-		System.out.println("\n--1d6 rolado " + (n1+n2+n3+n4+n5+n6) + " vezes");
+		System.out.println("Quantos dados serao rolados?");
+		int numeroDados = sc.nextInt();
+
+		System.out.println("Quantos lados terao os dados?");
+		int ladosDados = sc.nextInt();
+
+		System.out.println();
+		System.out.println("--Rolando " + numeroDados + "d" + ladosDados + "--");
+		System.out.println();
 
 
+		ArrayList<Integer> rolagensDado = new ArrayList<Integer>();
+		for(int i=0; i<numeroDados; i++) {
+			int rolagem = rng.nextInt(ladosDados)+1;
+			rolagensDado.add(rolagem);
+			// System.out.println(rolagem);
+		}
+
+		String[] graphArr = new String[ladosDados+1];
+
+		for (Integer rolagem : rolagensDado) {
+			if (graphArr[rolagem] == null) graphArr[rolagem] = "";
+			graphArr[rolagem] += "■";
+		}
+
+		for (int i = 1; i < graphArr.length; i++) {
+			System.out.println(i + ": " + graphArr[i] + " [" + graphArr[i].length() + "]");
+		}
 	}
-
 }
